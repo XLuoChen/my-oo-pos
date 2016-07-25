8b9d7e32cf997a3d72fe6986720490484050c5f2
Hao Hu <hhu@thoughtworks.com>
2016-07-25 15:40:41 +0800

    move buildReceiptText to receipt.buildText method

--- src/models/receipt.js
+++ src/models/receipt.js
@@ -6,18 +6,42 @@ class Receipt {
     this.total = total;
   }
 
-  static buildReceipt(receiptItems) {
+  buildText() {
 
-  let total = 0;
-  let savedTotal = 0;
+    const formatMoney = (money) => {
+      return money.toFixed(2);
+    };
 
-  for (const receiptItem of receiptItems) {
-    total += receiptItem.subtotal;
-    savedTotal += receiptItem.saved;
+    let receiptItemsText = this.receiptItems
+      .map(receiptItem => {
+        const cartItem = receiptItem.cartItem;
+        return `名称：${cartItem.item.name}，\
+数量：${cartItem.count}${cartItem.item.unit}，\
+单价：${formatMoney(cartItem.item.price)}(元)，\
+小计：${formatMoney(receiptItem.subtotal)}(元)`;
+      })
+      .join('\n');
+
+    return `***<没钱赚商店>收据***
+${receiptItemsText}
+----------------------
+总计：${formatMoney(this.total)}(元)
+节省：${formatMoney(this.savedTotal)}(元)
+**********************`;
   }
 
-  return new Receipt(receiptItems, savedTotal, total);
-}
+  static buildReceipt(receiptItems) {
+
+    let total = 0;
+    let savedTotal = 0;
+
+    for (const receiptItem of receiptItems) {
+      total += receiptItem.subtotal;
+      savedTotal += receiptItem.saved;
+    }
+
+    return new Receipt(receiptItems, savedTotal, total);
+  }
 }
 
 module.exports = Receipt;
