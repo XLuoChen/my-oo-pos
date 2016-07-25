59df5683749471330e1078fdedfa13d33dffde7a
XLuoChen <1425145741@qq.com>
2016-07-25 20:50:17 +0800

    create class Receipt

---
+++ src/models/receipt.js
@@ -0,0 +1,9 @@
+class Reciept {
+  constructor(receiptItems = [], total = 0, savedTotal = 0) {
+    this.receiptItems = receiptItems;
+    this.total = total;
+    this.savedTotal = savedTotal;
+  }
+}
+
+module.exports = Reciept;
