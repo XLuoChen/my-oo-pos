8b57f252211acdc4089b48523c4ea491de3e4da1
XLuoChen <1425145741@qq.com>
2016-07-25 21:52:05 +0800

    package buildReceipt to receipt.js

--- src/models/receipt.js
+++ src/models/receipt.js
@@ -1,9 +1,18 @@
-class Reciept {
-  constructor(receiptItems = [], total = 0, savedTotal = 0) {
+class Receipt {
+  constructor(receiptItems = []) {
+
+    let total = 0;
+    let savedTotal = 0;
+
+    for (const receiptItem of receiptItems) {
+      total += receiptItem.subtotal;
+      savedTotal += receiptItem.saved;
+    }
+
     this.receiptItems = receiptItems;
     this.total = total;
     this.savedTotal = savedTotal;
   }
 }
 
-module.exports = Reciept;
+module.exports = Receipt;
